467ff2eaffc04b931b6ad0c186785eaeffeb63ba
rehnberg <danne.rehnberg@hotmail.com>
2015-04-06 16:20:05 +0200

    Daily commit - PostfixParser, PostfixParserTest

--- src/calculator/PostfixParser.java
+++ src/calculator/PostfixParser.java
@@ -7,36 +7,134 @@ import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class PostfixParser {
+	private enum Operators {
+		ADDITION("+", 1), 
+		SUBTRACTION("-", 1), 
+		MULTIPLICATION("*", 2), 
+		DIVISON("/", 2),
+		START_PARENTHESIS("(", 3),
+		END_PARENTHESIS(")", 3);
+		
+		private String symbol;
+		private int priority;
+		
+		private Operators(String symbol, int priority) {
+			this.symbol = symbol;
+			this.priority = priority;
+		}
+		
+		public String getSymbol() { return symbol; }
+		public int getPriority() { return priority; }
+		
+		public static Operators getFromSymbol(String symbol) {
+			for (Operators operator : values()) {
+				if (operator.symbol.equals(symbol))
+					return operator;
+			}
+			throw new IllegalArgumentException("No such operator: " + symbol);
+		}
+		
+		public static boolean isOperator(String symbol) {
+			for (Operators operator : values()) {
+				if (operator.symbol.equals(symbol))
+					return true;
+			}
+			return false;
+		}
+		
+	}
+
 	List<String> input;
 	List<String> output;
+	Deque<Operators> operatorStack = new ArrayDeque<Operators>();
+	String operatorString;
 	
 	public PostfixParser() {
 		input = new Vector<String>();
 		output = new Vector<String>();
+		initOperatorString();
 	}
 	
 	public List<String> getPostFix(String infix) {
-		StringTokenizer tokenizer = new StringTokenizer(infix, "+", true);
-		while(tokenizer.hasMoreTokens()) {
-			input.add(tokenizer.nextToken());
-		}
+		input.clear();
+		output.clear();
+		tokenize(infix);
 		parse();
 		return output;
 	}
 	
+	private void initOperatorString() {
+		StringBuilder builder = new StringBuilder();
+		for (Operators op : Operators.values()) {
+			builder.append(op.getSymbol());
+		}
+		operatorString = builder.toString();
+	}
+	
+	private void tokenize(String infix) {
+		StringTokenizer tokenizer = new StringTokenizer(infix, operatorString, true);
+		while(tokenizer.hasMoreTokens()) {
+			input.add(tokenizer.nextToken());
+		}
+	}
+	
 	private void parse() {
-		Deque<String> operatorStack = new ArrayDeque<String>();
+		
 		for (String string : input) {
-			if (string.equals("+")) {
-				operatorStack.addFirst(string);
+			if (Operators.isOperator(string)) {
+				handleAsOperator(string);
 			}
 			else {
 				output.add(string);
 			}
 		}
-		while(!operatorStack.isEmpty()) {
-			output.add(operatorStack.peekFirst());
-			operatorStack.removeFirst();
+		popAllOperators();
+	}
+	
+	private void handleAsOperator(String operator) {
+		Operators op = Operators.getFromSymbol(operator);
+		if (op.equals(Operators.END_PARENTHESIS)) {
+			handleEndParenthesis();
 		}
+		else {
+			addOperator(op);
+		}	
+	}
+	
+	private void handleEndParenthesis() {
+		while(true) {
+			Operators op = operatorStack.peekFirst();
+			if (op.equals(Operators.START_PARENTHESIS)) {
+				operatorStack.removeFirst();
+				break;
+			}
+			popTopOperator();
+		}
+	}
+	
+	private void addOperator(Operators op) {
+		if (!operatorStack.isEmpty()) {
+			if (operatorShouldBePopped(op)) {
+				popTopOperator();
+			}
+		}
+		operatorStack.addFirst(op);
+	}
+	
+	private boolean operatorShouldBePopped(Operators op) {
+		Operators topOperator = operatorStack.peekFirst();
+		return (topOperator != Operators.START_PARENTHESIS) && 
+		(op.getPriority() <= operatorStack.peekFirst().getPriority());
+	}
+	
+	private void popAllOperators() {
+		while(!operatorStack.isEmpty()) {
+			popTopOperator();
+		}
+	}
+	
+	private void popTopOperator() {
+		output.add(operatorStack.peekFirst().getSymbol());
+		operatorStack.removeFirst();
 	}
 }
--- test/calculator/PostfixParserTest.java
+++ test/calculator/PostfixParserTest.java
@@ -29,4 +29,65 @@ public class PostfixParserTest {
 		assertEquals(expectedResult, parser.getPostFix("5+6+8"));
 	}
 
+	@Test
+	public void testSimpleSubtraction() {
+		expectedResult = Arrays.asList("5", "6", "-");
+		assertEquals(expectedResult, parser.getPostFix("5-6"));
+	}
+	
+	@Test
+	public void testDoubleSubtraction() {
+		expectedResult = Arrays.asList("5", "6", "-", "8", "-");
+		assertEquals(expectedResult, parser.getPostFix("5-6-8"));
+	}
+	
+	@Test
+	public void testAdditionSubtractionPriority() {
+		expectedResult = Arrays.asList("6", "5", "+", "4", "-");
+		assertEquals(expectedResult, parser.getPostFix("6+5-4"));
+		expectedResult = Arrays.asList("6", "5", "-", "4", "+");
+		assertEquals(expectedResult, parser.getPostFix("6-5+4"));
+	}
+	
+	@Test
+	public void testSimpleMultiplication() {
+		expectedResult = Arrays.asList("16", "45", "*");
+		assertEquals(expectedResult, parser.getPostFix("16*45"));
+	}
+	
+	@Test
+	public void testDoubleMultiplication() {
+		expectedResult = Arrays.asList("16", "45", "*", "19", "*");
+		assertEquals(expectedResult, parser.getPostFix("16*45*19"));
+	}
+	
+	@Test
+	public void testSimpleDivision() {
+		expectedResult = Arrays.asList("16", "45", "/");
+		assertEquals(expectedResult, parser.getPostFix("16/45"));
+	}
+	
+	@Test
+	public void testDoubleDivision() {
+		expectedResult = Arrays.asList("16", "45", "/", "19", "/");
+		assertEquals(expectedResult, parser.getPostFix("16/45/19"));
+	}
+	
+	@Test
+	public void testTripleDivision() {
+		expectedResult = Arrays.asList("16", "45", "/", "19", "/", "88", "/");
+		assertEquals(expectedResult, parser.getPostFix("16/45/19/88"));
+	}
+	
+	@Test
+	public void testPriorities() {
+		expectedResult = Arrays.asList("6","2","/","1","2","+","*");
+		assertEquals(expectedResult, parser.getPostFix("6/2*(1+2)"));
+	}
+	
+	@Test
+	public void testSimpleParenthesis() {
+		expectedResult = Arrays.asList("4", "6", "+", "5", "*");
+		assertEquals(expectedResult, parser.getPostFix("(4+6)*5"));
+	}
 }
